0b630d147c33d0f46b679f0ffc14c4b0d9165e03
Rafael Bartz <infobartz@gmail.com>
2021-12-09 16:18:22 -0300

    Feito melhorias no design da página.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,18 +10,22 @@ const RESOURCES = {
 "assets/assets/logo_windows.png": "1c164bc52d26883fc9493916b8307c7c",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "27f11f24fa6cc676cce76e0e9fb12e86",
+"assets/NOTICES": "5af975dc4a0c4f41b48dae3886c6726e",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0e1d2a4d5c36280f7fc4fcacf01ba0a8",
-"/": "0e1d2a4d5c36280f7fc4fcacf01ba0a8",
-"main.dart.js": "6298dd03b228603a6ea3d8be16357b82",
+"index.html": "45807dbe043d1c3a6a6dc63c27c10f01",
+"/": "45807dbe043d1c3a6a6dc63c27c10f01",
+"main.dart.js": "57ca6e78dce4fa27861e5222c1b48415",
 "manifest.json": "de1ac2a1f1e3a86f207803ddd4f0f82d",
-"version.json": "83717863733eab1ac47db133c27d2852"
+"version.json": "70e66df1c68b72c8c1811b415c8cc1fe"
 };
 
 // The application shell files that are downloaded before a service worker can
